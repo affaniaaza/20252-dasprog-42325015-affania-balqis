eb402525aabe0d047b212255114812e1904ecc49
Affania Fariza Balqis <42325015@mt.peradaban.ac.id>
2026-05-18 00:05:24 +0700

    P6-1-Menerapkan fungsi untuk memproses input dan menampilkannya di layar monitor.

--- app/hitung-lingkaran/page.tsx
+++ app/hitung-lingkaran/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -26,28 +27,42 @@ const formSchema = z.object({
   radius: z
     .string()
     .min(1, "Radius Lingkaran harus diisi")
-    .regex(/^-?\d+(\.\d+)?$/, "Radius Lingkaran harus berupa angka")
-    .regex(/^[^-]/, "Radius Lingkaran harus berupa angka positif")
-    .regex(/^(?!0+(\.0+)?$).$/, "Radius Lingkaran harus lebih besar dari nol")
+    .regex(/^\d+(\.\d+)?$/, "Radius harus berupa angka positif")
+    .regex(/^(?!0+(\.0+)?$)\d+(\.\d+)?$/, "Radius tidak boleh nol")
     .transform(Number),
 });
 
 const HitungLingkaran = () => {
   //State (kondisi/status) untuk menyimpan hasil perhitungan luas dan keliling lingkaran serta status apakah hasil perhitungan sudah ditampilkan atau belum
-  const [radius, setRadius] = useState("");
+  const [radius, setRadius] = useState(0);
   const [luas, setLuas] = useState(0);
   const [keliling, setKeliling] = useState(0);
   const [sdhDihitung, setSdhDihitung] = useState(false);
 
-  // Inisialisasi form
+  // Inisialisasi form menggunakan useForm dari @tanstack/react-form dengan konfigurasi default values, validators, dan onSubmit handler.
   const form = useForm({
     // Menentukan default values untuk masing-masing field dalam formulir.
     defaultValues: { radius: "" },
     // Menentukan validasi untuk setiap field dalam formulir menggunakan zod.
-    validators: { onSubmit: formSchema },
+    validators: { onBlur: formSchema },
     // Menentukan fungsi yang akan dipanggil ketika formulir disubmit.
-    onSubmit: () => {},
+    onSubmit: ({ value }) => {
+      console.log(value);
+
+      const r = Number(value.radius); // Mengambil nilai radius dari form dan mengkonversinya menjadi angka untuk digunakan dalam perhitungan luas dan keliling lingkaran
+      const hitungLuas = Math.PI * r * r; // Rumus luas lingkaran: πr²
+      const hitungKeliling = 2 * Math.PI * r; // Rumus keliling lingkaran: 2πr
+
+      console.log("Luas Lingkaran:", hitungLuas);
+      console.log("Keliling Lingkaran:", hitungKeliling);
+
+      setRadius(r);
+      setLuas(hitungLuas);
+      setKeliling(hitungKeliling);
+      setSdhDihitung(true); // Menandakan bahwa perhitungan sudah dilakukan sehingga hasilnya dapat ditampilkan di UI
+    },
   });
+
   return (
     <div className="flex justify-center items-center min-h-screen">
       <Card className="w-2/3">
@@ -60,7 +75,7 @@ const HitungLingkaran = () => {
           <form
             id="hitung-lingkaran-form"
             onSubmit={(e) => {
-              e.preventDefault();
+              e.preventDefault(); // Mencegah perilaku default form yang akan me-refresh halaman saat disubmit
               form.handleSubmit();
             }}
           >
@@ -101,8 +116,45 @@ const HitungLingkaran = () => {
           </form>
         </CardContent>
         <Separator />
+        {sdhDihitung && (
+          <>
+            <div className="px-6 text-orange-700 rounded-lg">
+              <h1 className="font-semibold mb-3">
+                Hasil Kalkulasi Lingkaran Radius {""}
+                {radius.toLocaleString("id-ID", {
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 2,
+                })}{" "}
+                cm
+              </h1>
+              <p>
+                luas: {""}
+                {luas.toLocaleString("id-ID", {
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 2,
+                })}{" "}
+                {""} cm<sup>2</sup>, keliling: {""}
+                {keliling.toLocaleString("id-ID", {
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 2,
+                })}{" "}
+                cm
+              </p>
+            </div>
+            <Separator />
+          </>
+        )}
         <CardFooter className="flex gap-2">
-          <Button className="rounded-lg">Reset</Button>
+          <Button
+            type="button"
+            onClick={() => {
+              form.reset();
+              setSdhDihitung(false);
+            }}
+            className="rounded-lg"
+          >
+            Reset
+          </Button>
           <Button
             type="submit"
             form="hitung-lingkaran-form"
